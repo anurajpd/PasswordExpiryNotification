8327add64c830343eb0c3a3b01267f3be81cfeb4
Anuraj PD <73549337+anurajpd@users.noreply.github.com>
2020-10-28 21:57:29 +0300

    Updated with configuration file creation

--- test.ps1
+++ test.ps1
@@ -1,3 +1,42 @@
-$notifiedUsers = [System.Collections.ArrayList]@('hai', 'good', 'boy')
-
-Write-Output ($notifiedUsers -join '</br>')
+function New-ARConfigurationFile{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [hashtable]$ConfigParam,
+        [Parameter(Mandatory=$true)]
+        [string]$ConfigFile
+    )
+    New-Item $ConfigFile -Force -ItemType File | Out-Null
+    Set-Content -Path $ConfigFile -Value '<?xml version="1.0"?>'
+    Add-Content -Path $ConfigFile -Value '<Settings></Settings>'
+    $xmlConfig = New-Object System.Xml.XmlDocument
+    $xmlConfig.load($ConfigFile)
+    foreach($param in $ConfigParam.Keys){
+        $paramNode = $xmlConfig.CreateElement($param)
+        $xmlConfig.SelectSingleNode('/Settings').AppendChild($paramNode) | Out-Null
+    }
+    $xmlConfig.Save($configFile)
+}
+### Processing configuration file
+# Configuration Parameters expected in Settings.xml file.
+$configParams = @{
+    'Domain' = ''
+    'ADServer' = ''
+    'InitialNotifyDays' = '15'
+    'DailyNotifyDays' = '5'
+    'SMTPServer' = ''
+    'SMTPPort' = '25'
+    'SMTPUseSSL' = 'false'
+    'MailFrom' = ''
+    'MailSubject' = 'Password Expiry Notification'
+    'AdminMail' = ''
+    'MailUser' = ''
+    'MailPassword' = ''
+    'ADUser' = ''
+    'ADPassword' = ''
+}
+$configFile = '.\Settings.xml'
+if(!(Test-Path $configFile)){
+    New-ARConfigurationFile -ConfigFile $configFile -ConfigParam $configParams
+}
